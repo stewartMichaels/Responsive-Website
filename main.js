04c08dd042884b31ed9736cf46384c9713ac024c
Stewart Michaels <steward.kama@gmail.com>
2023-02-27 14:23:35 +0530

    last fixes

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const mobileMenu = document.querySelector("nav .mobile-menu");
 const mobileClose = document.querySelector("nav .mobile-close");
 const mobileNav = document.querySelector(".mobile-nav");
+const mobileNavLiAll = document.querySelector("nav.mobile-nav ul li");
+const socials = document.querySelectorAll(".socials a img")
+const desktopNavLiAll = document.querySelectorAll("nav ul li")
 
 mobileMenu.addEventListener("click", () => {
     mobileNav.classList.add("active");
@@ -10,4 +13,16 @@ mobileMenu.addEventListener("click", () => {
 mobileClose.addEventListener("click", () => {
     mobileNav.classList.remove("active");
     mobileClose.classList.remove("active");
+})
+
+mobileNavLiAll.forEach((li, i) => {
+    li.style.animationDelay = 400 + i * 140 + "ms";
+})
+
+socials.forEach((li, i) => {
+    li.style.animationDelay = 1400 + i * 120 + "ms";
+})
+
+desktopNavLiAll.forEach((li, i) => {
+    li.style.animationDelay = 2000 + i * 140 + "ms";
 })
